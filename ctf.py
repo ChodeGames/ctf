7d9eac9647cf453b09012fd5b5bb7b5b103dd1da
Matt Parlette <matthew.parlette@gmail.com>
2014-03-25 20:37:00 -0400

    Created a 4x4 grid of grass

---
+++ ctf.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+
+class CTFGame(Widget):
+    pass
+
+class CTFApp(App):
+    def build(self):
+        layout = GridLayout(cols=4,rows=4)
+        for i in xrange(0,16):
+            layout.add_widget(Image(source='grass.jpg'))
+        return layout
+
+if __name__ == '__main__':
+    CTFApp().run()
