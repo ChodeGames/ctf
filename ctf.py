4952a80465468d4f971b98dec3b0ebfcc6165c54
Matt Parlette <matthew.parlette@gmail.com>
2014-03-27 10:26:16 -0400

    Trying a different method for creating background.
    Also adds player object on the board

--- ctf.py
+++ ctf.py
@@ -3,19 +3,30 @@ from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.graphics import Rectangle,Color
+from kivy.graphics.instructions import Canvas
 
 class CTFGame(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super(CTFGame, self).__init__(**kwargs)
+        self.grass_texture = Image(source='grass.jpg').texture
+        self.player_texture = Image(source='player.jpg').texture
+
+        with self.canvas:
+            self.board = Rectangle(texture=self.grass_texture,pos=self.pos,size=self.size)
+            self.player = Rectangle(texture=self.player_texture,pos=self.center,size=(50,50))
+        self.bind(size=self.update_size)
+
+    def update_size(self, *largs):
+        self.board.size = self.size
+
+    def player_move(self):
+        pass
+
 
 class CTFApp(App):
     def build(self):
-        layout = GridLayout(cols=4,rows=4)
-        for i in xrange(0,16):
-            if i is 0:
-                layout.add_widget(Image(source='player.jpg'))
-            else:
-                layout.add_widget(Image(source='grass.jpg'))
-        return layout
+        return CTFGame()
 
 if __name__ == '__main__':
     CTFApp().run()
